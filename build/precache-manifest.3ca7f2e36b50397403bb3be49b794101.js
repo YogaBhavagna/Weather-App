40522303f1aeed2004aa2f6dd8d6e89704755a0f
Yoga Bhavagna Jonnala <72809470+YogaBhavagna@users.noreply.github.com>
2022-12-02 22:35:37 +0530

    Add files via upload

---
+++ build/precache-manifest.3ca7f2e36b50397403bb3be49b794101.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cfdf862cc857eda44fa8",
+    "url": "/static/css/main.fe31e93a.chunk.css"
+  },
+  {
+    "revision": "cfdf862cc857eda44fa8",
+    "url": "/static/js/main.cfdf862c.chunk.js"
+  },
+  {
+    "revision": "80b3acba20a1c2c41809",
+    "url": "/static/css/1.868f1677.chunk.css"
+  },
+  {
+    "revision": "80b3acba20a1c2c41809",
+    "url": "/static/js/1.80b3acba.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4bf0dcc694787da19bb73adc390ce3f3",
+    "url": "/index.html"
+  }
+];
